a7d4fbfd21eab1779458f663ef3554c60d461771
Ardi <ardinurinsan03@gmail.com>
2022-07-15 21:40:02 +0700

    FIX : fix page welcome index website

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => "<h1>Hello everyone 👋, the API forum app has been released !</h1>");
